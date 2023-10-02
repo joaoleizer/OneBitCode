06ec85b771ee86a84323862fc4e3e8ff462b4775
João Vitor Leizer Antonow Centeno <jv.leizer.a@gmail.com>
2023-10-02 10:46:38 -0300

    Treinando usar js no html
    utilizando prompt para perguntar o dados e armazenar em uma variável e depois usando alert para mostrar os valores armazenados

---
+++ ex01-02Js/Recruta/index.js
@@ -0,0 +1,10 @@
+let firstName = prompt("Qual o seu primeiro nome? ")
+let lastName = prompt("Qual o seu sobrenome? ")
+let studying = prompt("Qual o seu campo de estudo? ")
+let yearOfBirth = prompt("Qual o ano de nascimento? ")
+
+alert("Recruta cadastrado com sucesso" +
+"\nNome completo: " + firstName + " " + lastName +
+"\nCampo de estudo: " + studying +
+"\nIdade: " + (2023 - yearOfBirth)
+)
