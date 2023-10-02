720b12fd1b22f8441d56296902a7c99d7244c419
João Vitor Leizer Antonow Centeno <jv.leizer.a@gmail.com>
2023-10-02 10:55:41 -0300

    fazendo uma calculadora
    Treinando mais um pouco os comandos e utilizando o parseFloat para pegar o valor do prompt que vem como uma string e transformando em float

---
+++ ex01-02Js/calculadora/index.js
@@ -0,0 +1,15 @@
+let number1 = parseFloat(prompt("Digite o primeiro número: "))
+let number2 = parseFloat(prompt("Digite o Segundo número: "))
+
+let sum = number1 + number2
+let subtraction = number1 - number2
+let multiplication = number1 * number2
+let division = number1 / number2
+
+alert(
+    "Resultados: " +
+    "\nSoma: " + sum +
+    "\nSubtração: " + subtraction +
+    "\nDivisão: " + division +
+    "\nMultiplicação: " + multiplication
+)
