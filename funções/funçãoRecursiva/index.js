1121d3f9943f38360f0d5897edde314bf2b3f7c9
João Vitor Leizer Antonow Centeno <jv.leizer.a@gmail.com>
2023-10-18 09:47:29 -0300

    função recursiva
    Treinando a utilização de função recursiva

---
+++ funções/funçãoRecursiva/index.js
@@ -0,0 +1,24 @@
+function dividir(num) {
+    console.log(num)
+    if (num % 2 === 0) {
+        dividir(num / 2)
+    }else {
+        return num
+    }
+}
+
+//dividir(27)
+
+function fatorial(num) {
+    console.log("num = " + num)
+    if (num === 0) {
+        return 1
+    } else if (num === 1) {
+        return 1
+    }else {
+        console.log(num + " * !" + (num -1))
+        return num * fatorial(num - 1)
+    }
+}
+
+console.log(fatorial(5))
