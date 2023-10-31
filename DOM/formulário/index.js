c939289f5eee64a1e6100d1431a3d48c25d6e785
João Vitor Leizer Antonow Centeno <jv.leizer.a@gmail.com>
2023-10-31 10:50:03 -0300

    Form
    Recebendo informações do formulário de forma dinâmica com JS, sem usar o action da tag form, envio os dados atraves do JS e usando o preventDefault() para que o formulário não tenha o evento padrão de recarregar a página

---
+++ DOM/formulário/index.js
@@ -0,0 +1,31 @@
+const form = document.getElementById('orderForm')
+
+form.addEventListener('submit', function (ev) {
+    ev.preventDefault()
+
+    const name = document.querySelector('input[name="name"]').value
+    const address = document.querySelector('input[name="address"]').value
+    const breadType = document.querySelector('select[name="breadType"]').value
+    const main = document.querySelector('input[name="main"]').value
+    const observations = document.querySelector('textarea').value
+
+    let salad = ""
+    document.querySelectorAll('input[name="salad"]:checked').forEach(function (item) {
+        salad += " - " + item.value + "\n"
+    })
+
+    const confirmation = confirm(
+        "Você confirma este pedido? " +
+        "\nNome do cliente: " + name +
+        "\nEndereço do cliente: " + address +
+        "\nTipo do pão: " + breadType +
+        "\nRecheio: \n -" + main + "\n" + salad +
+        "\nObservações: " + observations
+    )
+
+    if (confirmation) {
+        alert("Pedido realizado com sucesso!!")
+    } else {
+        alert("Pedido cancelado!!")
+    }
+})
